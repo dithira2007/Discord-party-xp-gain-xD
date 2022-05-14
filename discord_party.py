bd0f8a5fd58dc529d5870ca9fdbaed61ca5d9c3e
dithira2007 <66258991+dithira2007@users.noreply.github.com>
2022-05-14 09:06:41 +0530

    Main.py

---
+++ discord_party.py
@@ -0,0 +1,18 @@
+import pyautogui, time
+
+print("Discord party booted up")
+time.sleep(4)
+
+while True:
+    time.sleep(1)
+    pyautogui.typewrite("haha boi my achivements gonna rise. hehe knowing to code it so fking epic lmao ")
+    pyautogui.press("enter")
+    time.sleep(0.5)
+    pyautogui.typewrite("lmao this will continue until i rich the max xp boost hehe")
+    pyautogui.press("enter")
+    time.sleep(0.5)
+    pyautogui.typewrite("unluckily you had to bve one of my best friends leading your chat to be used as a bio toxic chat for xp boost")
+    pyautogui.press("enter")
+    time.sleep(0.5)
+    pyautogui.typewrite("btw if you need this code let me know i'll share it :))))))))))")
+    pyautogui.press("enter")
